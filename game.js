9dbbfc7590932122a700e1034190caba66230783
ShivaMoradi <shiva.moradi1@gmail.com>
2023-11-22 13:28:45 +0100

    defined word =[] and also fixed some error

--- game.js
+++ game.js
@@ -6,19 +6,18 @@ import Module from "node:module";
 import Scores from "./highScoreList.js";
 const require = Module.createRequire( import.meta.url );
 const prompt = require( 'prompt-sync' )( { sigint: true } );
-export default class Game{
 
+export default class Game{
   constructor () {
     console.log( "Welcome to the Hangman game! start the gane:" )
-    // creat gallow
-    let highScoreList = new Scores()
-    let gallow = new Gallow()
-     // 10. ask for  word                         bass
-    let question = new Question( "Type the secret word, don't show your opponent: " )
-    let secretWord = new SecretWord(question.answer)
-    console.log( "The secret word has " + secretWord.length )
     let player = new Player()
-  // player guss  a charector
+    let highScoreList = new Scores()
+    let word = new Question( "Type the secret word: " )
+    word = []
+    new SecretWord()
+    console.log( "The secret word has " + word.length )
+    let gallow = new Gallow()
+
     }
   }
   
