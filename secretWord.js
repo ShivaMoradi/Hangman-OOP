11a0cac1d8aeff1d076e81201f52f1f26db449cf
ShivaMoradi <shiva.moradi1@gmail.com>
2023-11-28 15:09:47 +0100

    expålained changes in previous commit

--- secretWord.js
+++ secretWord.js
@@ -1,15 +1,11 @@
-import Question from "./question.js"
-new Question()
-export default class SecretWord {
-  constructor () {
-    this.wordLength()
-  }
+import PromptSync from "prompt-sync"
+import promptWord from "./question.js";
+const prompt = PromptSync()
+let word = promptWord()
+export default function wordLength() {
   //word length to be able to create '-' /space to show how long is the word for player gusses
-  wordLength ( word ) {
-    word = []
     if ( word && word.length > 0 ) {
-      this.word = new Array( word.length ).fill( "-" )
-      return this.word
+      let secretWord = new Array( word.length ).fill( "-" )
+      return secretWord
     }
   }
-}
