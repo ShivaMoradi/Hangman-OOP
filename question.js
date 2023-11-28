b0190442c60e723a05eda8a375f757f13a87495d
ShivaMoradi <shiva.moradi1@gmail.com>
2023-11-28 15:07:09 +0100

    in question.js file changed classes to function and called back word from question.js

--- question.js
+++ question.js
@@ -1,11 +1,9 @@
-export default class Question{
-  constructor ( word ) {
-    this.word = word
-    this.answer = this.promptWord.toUpperCase
-  }
-  promptWord () {
+import PromptSync from "prompt-sync"
+const prompt = PromptSync()
+
+export default function promptWord(){
   
-    let word = prompt( "Type the secret word: " )
-}
+  let word = prompt( "Type the secret word: " )
+  return word
 
 }
