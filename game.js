530c26fb58e579bda1f62398b1059b2e2ba24b3c
ShivaMoradi <shiva.moradi1@gmail.com>
2023-12-21 16:25:10 +0100

    desided to move if conditions and loops from player.js to game.js

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 import Module from "node:module";
-//import Scores from "./highScoreList.js";
 const require = Module.createRequire( import.meta.url );
 import promptWord from "./question.js";
 import wordLength from "./secretWord.js";
 import playerGussChar from "./player.js";
 //import ScoresList from "./highScoreList.js"
-import gallowSlots from "./gallow.js";
+import slot from "./gallow.js";
+import Gallow from "./gallow.js";
 
 
 export default async function Game () {
@@ -15,29 +15,27 @@ export default async function Game () {
 
 
   for ( let i = 0; i < word.length; i++ ) {
-    let char = new playerGussChar()
-  }
-    if ( word.includes( char ) ) {
-      let gussedWord = char
-      return gussedWord;
-     // console.log( "Congratulation! You gussed a correct Letters! Win!" + gussedWord )
+    // Display current state of guessed word or gallow slots
+    let char = []
+     char = new playerGussChar()
+    let gussedWord = char
+    console.log( "Guessed Word: " + gussedWord );
+    console.log( "Gallow Slots: " + slot);
 
+      if ( word.includes( char ) ) {
+        gussedWord += char
+        console.log( "Correct guess! Guessed Word: " + gussedWord );
+
+    } else {
+      // Update the gallow slots
+      slot += "_"; // For each incorrect guess, you might add a part of the hangman
+      console.log( "Incorrect guess! Gallow Slots: " + slot );
     }
-
-  else {
-      let slot = new gallowSlots()
-      return slot;
-    }
-    
-    //console.log( "Game Over!"  + gallow.slot())
-
-
   }
 
-  
-
-
-  //new Scores()
+  // Game over message
+  console.log( "Game Over!" );
+}
 
 
 
